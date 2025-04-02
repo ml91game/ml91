4d0191e53136951c0e8fac0f910373740dc6b1c1
funfun <funfun@gmail.com>
2025-04-02 15:05:14 +0800

    c

---
+++ qjrxgm/version.lua
@@ -0,0 +1,16 @@
+
+
+
+return {
+    patch = 0,
+    url = "http://qjrx-res.ffsunrui.com.cn/patch",
+    getLoginUrl = function()
+        return "http://159.138.22.23:16666"
+    end,
+    getNoticeUrl = function()
+        return "http://159.138.22.23:18080/notice"
+    end,
+    getSubpackUrl = function()
+        return "https://qjrx-res.ffsunrui.com.cn/v/"
+    end,
+}
