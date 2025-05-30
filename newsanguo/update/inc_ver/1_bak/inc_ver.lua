f653306a47414210dd6e24ee59c669d653cd475d
funfun <funfun@gmail.com>
2025-05-30 17:49:14 +0800

    c

--- newsanguo/update/inc_ver/1_bak/inc_ver.lua
+++
@@ -1,8 +0,0 @@
-return {
-	["src/game/action/view/action_open_server_gift_window.luac"] = {
-		file = "action_open_server_gift_window.luac", ver = 1, size = 12673, },
-	["src/game/newfirstcharge/view/bigfirstcharge_window.luac"] = {
-		file = "bigfirstcharge_window.luac", ver = 1, size = 6745, },
-	["src/config/login_days_new_data.luac"] = {
-		file = "login_days_new_data.luac", ver = 1, size = 2673, },
-}
